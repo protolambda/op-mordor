121e90698a312b8b8f965fc1431cf4cc4c8704e7
Sebastian Stammler <iam@decentr.al>
2023-02-09 10:49:59 -0600

    Implement more L2Sugar

--- l2_sugar.go
+++ l2_sugar.go
@@ -5,23 +5,33 @@ import (
 	"log"
 
 	"github.com/ethereum-optimism/optimism/op-node/eth"
+	"github.com/ethereum-optimism/optimism/op-node/rollup"
+	"github.com/ethereum-optimism/optimism/op-node/rollup/derive"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
 type L2Sugar struct {
-	oracle L2PreimageOracle
-	ctx    context.Context
+	oracle  L2PreimageOracle
+	cfg     *rollup.Config
+	genesis *rollup.Genesis
+	ctx     context.Context
 
 	head   eth.BlockInfo
 	blocks map[common.Hash]*types.Block
 }
 
-func NewL2Sugar(head eth.BlockInfo, oracle L2PreimageOracle) *L2Sugar {
+func NewL2Sugar(
+	head eth.BlockInfo,
+	oracle L2PreimageOracle,
+	cfg *rollup.Config,
+	genesis *rollup.Genesis,
+) *L2Sugar {
 	return &L2Sugar{
+		oracle: oracle,
+		cfg:    cfg,
 		ctx:    context.TODO(),
 		head:   head,
-		oracle: oracle,
 	}
 }
 
@@ -42,9 +52,13 @@ func (l *L2Sugar) getBlockInfoByHash(hash common.Hash) eth.BlockInfo {
 }
 
 func (l *L2Sugar) getHeaderByHash(hash common.Hash) *types.Header {
+	return l.getBlockByHash(hash).Header()
+}
+
+func (l *L2Sugar) getBlockByHash(hash common.Hash) *types.Block {
 	block, ok := l.blocks[hash]
 	if ok {
-		return block.Header()
+		return block
 	}
 
 	block, err := l.oracle.FetchL2Block(l.ctx, hash)
@@ -54,7 +68,7 @@ func (l *L2Sugar) getHeaderByHash(hash common.Hash) *types.Header {
 	}
 	l.blocks[hash] = block
 
-	return block.Header()
+	return block
 }
 
 func (l *L2Sugar) getBlockHashByNumber(u uint64) common.Hash {
@@ -84,11 +98,17 @@ func (l *L2Sugar) L2BlockRefByLabel(ctx context.Context, label eth.BlockLabel) (
 }
 
 func (l *L2Sugar) L2BlockRefByHash(ctx context.Context, l2Hash common.Hash) (eth.L2BlockRef, error) {
-	//TODO implement me
-	panic("implement me")
+	payload, err := l.PayloadByHash(ctx, l2Hash)
+	if err != nil {
+		return eth.L2BlockRef{}, err
+	}
+	return derive.PayloadToBlockRef(payload, l.genesis)
 }
 
 func (l *L2Sugar) SystemConfigByL2Hash(ctx context.Context, hash common.Hash) (eth.SystemConfig, error) {
-	//TODO implement me
-	panic("implement me")
+	payload, err := l.PayloadByHash(ctx, hash)
+	if err != nil {
+		return eth.SystemConfig{}, err
+	}
+	return derive.PayloadToSystemConfig(payload, l.cfg)
 }
